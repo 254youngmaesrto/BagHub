732df1b1f3ff31b063c8e9aa45a16d1a82cf8a90
254youngmaesrto <spydermaestro@gmail.com>
2026-05-05 17:49:27 +0300

    Fix User model import to use custom api.User

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate  # <--- FIXED: Moved from rest_framework
 from django.db import transaction
 from django.utils import timezone
